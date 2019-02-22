6f3e96d3d457f60511f174d2979bbb4f9b6bc8be
Sergei Zelinsky <sergei.zelinsky.dev@gmail.com>
2019-02-22 02:30:50 +0200

    sequence-lights: components
    introduce Circle component

---
+++ src/components/Circle/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Image, ImageProps, ImageRequireSource, StyleSheet} from 'react-native';
+
+export interface ICircleProps
+  extends Pick<ImageProps, Exclude<keyof ImageProps, 'source'>> {
+  active: boolean;
+  activeImageSource: ImageRequireSource;
+  inactiveImageSource: ImageRequireSource;
+  name: string;
+}
+
+const Circle: React.FunctionComponent<ICircleProps> = ({
+  active,
+  activeImageSource,
+  inactiveImageSource,
+  style,
+  ...rest
+}: ICircleProps) => (
+  <Image
+    style={[styles.root, style]}
+    source={active ? activeImageSource : inactiveImageSource}
+    {...rest}
+  />
+);
+
+const styles = StyleSheet.create({
+  root: {
+    width: 72,
+    height: 72,
+  },
+});
+
+export default Circle;
